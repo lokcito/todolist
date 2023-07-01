0ddf05cc9b2e3bb452474f76c2ab9f0cc8232792
Ray Rojas <me@rayrojas.info>
2023-06-30 20:31:14 -0500

    base de datos definida

--- todolist/settings.py
+++ todolist/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edutecno_todolist',
+        'USER': 'edutecno_test',
+        'PASSWORD': "%123456%",
+        'HOST': 'postgresql-edutecno.alwaysdata.net',
+        'PORT': '5432',
     }
 }
 
