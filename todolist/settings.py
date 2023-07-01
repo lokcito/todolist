443a1800934b9e62275a46e47661dba704817955
Ray Rojas <me@rayrojas.info>
2023-07-01 10:23:37 -0500

    listo para desplegar

--- todolist/settings.py
+++ todolist/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos',
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
